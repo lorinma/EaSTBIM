4544a28364a94acb2522fd2b21e027864ecae8cd
Ling Ma <malingreal@gmail.com>
2015-11-03 09:31:41 +0200

    add fcl,octomap,libccd

--- main.cpp
+++ main.cpp
@@ -27,22 +27,73 @@
 
 #include "IO.h"
 #include "Visualizer.h"
+//TODO operator class check relation
+//TODO convert topo to mesh
+
+//#include <fcl/shape/geometric_shapes.h>
+//#include <fcl/shape/geometric_shapes_utility.h>
+//#include <fcl/narrowphase/narrowphase.h>
+//#include <fcl/collision.h>
+//#include <boost/foreach.hpp>
+
+//using namespace fcl;
+using namespace std;
 int main (int argc, char** argv)
 {
-    IO data;
-    boost::ptr_vector<string> ifcfiles;
-    data.GetIfcFiles(ifcfiles);
-    BIM::Ptr bim(new BIM);
-    data.LoadIfcModel(ifcfiles.at(0),bim);
+//    IO data;
+//    boost::ptr_vector<string> ifcfiles;
+//    data.GetIfcFiles(ifcfiles);
+//    BIM::Ptr bim(new BIM);
+//    data.LoadIfcModel(ifcfiles.at(0),bim);
+//
+//    Visualizer viewer;
+//    for (BIM::Element_set::iterator it=bim->elements.begin();it!=bim->elements.end();++it){
+//        Box box;
+//        it->GetMVBB(box);
+//        viewer.AddTopoShapeWithProperty(it->shape);
+//        viewer.AddBoxWithProperty(box,255);
+//    }
+//    viewer.spin();
+//
 
-    Visualizer viewer;
-    for (BIM::Element_set::iterator it=bim->elements.begin();it!=bim->elements.end();++it){
-        Box box;
-        it->GetMVBB(box);
-        viewer.AddTopoShapeWithProperty(it->shape);
-        viewer.AddBoxWithProperty(box,255);
-    }
-    viewer.spin();
+//    boost::shared_ptr<fcl::Box> box0(new fcl::Box(1,1,1));
+//    boost::shared_ptr<fcl::Box> box1(new fcl::Box(1,1,1));
+//    fcl::GJKSolver_libccd solver;
+//    fcl::Vec3f contact_points;
+//    fcl::FCL_REAL penetration_depth;
+//    fcl::Vec3f normal;
+//
+//    fcl::Transform3f tf0, tf1;
+//    tf0.setIdentity();
+//    tf0.setTranslation(fcl::Vec3f(.9,0,0));
+//    tf0.setQuatRotation(fcl::Quaternion3f(.6, .8, 0, 0));
+//    tf1.setIdentity();
+//
+//    bool res = solver.shapeIntersect(*box0, tf0, *box1, tf1, &contact_points, &penetration_depth, &normal);
+//
+//    cout << "contact points: " << contact_points << endl;
+//    cout << "pen depth: " << penetration_depth << endl;
+//    cout << "normal: " << normal << endl;
+//    cout << "result: " << res << endl;
+//
+//    static const int num_max_contacts = std::numeric_limits<int>::max();
+//    static const bool enable_contact = true;
+//    fcl::CollisionResult result;
+//    fcl::CollisionRequest request(num_max_contacts,
+//                                  enable_contact);
+//
+//
+//    fcl::CollisionObject co0(box0, tf0);
+//    fcl::CollisionObject co1(box1, tf1);
+//
+//    fcl::collide(&co0, &co1, request, result);
+//    vector<fcl::Contact> contacts;
+//    result.getContacts(contacts);
+//
+//    cout << contacts.size() << " contacts found" << endl;
+//    BOOST_FOREACH(fcl::Contact& contact, contacts) {
+//                    cout << "position: " << contact.pos << endl;
+//                }
 
 //    PointCloudPtr cloud(new PointCloud);
 //    data.ReadPts("damaged beam.pts",cloud);
